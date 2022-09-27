99919cb55ff639b061ae6cd9292189114dbd2b53
johan.estrada-p <johan.estrada-p@mail.escuelaing.edu.co>
2022-09-27 13:02:40 -0500

    First Commit

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ def prob(vec,n):
         norma+=abs(i)**2
     return ((abs(vec[n]**2)/norma))
 def prob_vec(vec,vec1):
-    normvec1=vec/numpy.linalg.norm(vec)
-    normvec2=vec1/numpy.linalg.norm(vec1)
-    prod_inter=numpy.inner()
+    norm1=numpy.linalg.norm(vec)
+    norm2=numpy.linalg.norm(vec1)
+    for i in range(len(vec)):
+        vec[i]=vec[i]*(1/norm1)
+    for i in range(len(vec1)):
+        vec1[i]=vec1[i]*(1/norm2)
+    vec1=numpy.matrix.getH(vec)
+
+    print(norm1)
+prob_vec([1+2j,2+2j],[1+1j,1])
+
 print(prob([(-3-1j),(-2j),(1j),(2)],2))
